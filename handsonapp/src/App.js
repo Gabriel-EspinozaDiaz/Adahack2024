37f59a5bf68530314f45ae9b52a95c52d5b5a49e
Mostafa Ebid <mebid99@gmail.com>
2024-10-19 12:33:46 +0100

    started making launch page, its styles

--- handsonapp/src/App.js
+++ handsonapp/src/App.js
@@ -9,22 +9,38 @@ import './App.css';
 
 function App() {
     return (
-      <div className="App">
-        <div>
-          <h1>Learn Biology with Interactive Minigames!!</h1>
+    <div className="launch-container">
+        <div className="floating-elements">
+          
+          {/* Floating elements like atoms, planets, and molecules */}
+          <div className="element atom"></div>
+          <div className="element dna"></div>
+          <div className="element molecule"></div>
+
         </div>
-        <div className="">
-            <div>
-                <Minigame1 />
-            </div>
-            <div>
-                <Minigame2 />
-            </div>
-            <div>
-                <Minigame3 />
-            </div>
+
+      <h1 className="header">Minigames</h1>
+
+      <div className="center-content">
+        <div>
+          <h1 className="game-logo">Minigame1</h1>
+          <p>Description</p>
+          <button className="start-button">Start Game</button>
+        </div>
+
+        <div>
+          <h1 className="game-logo">Minigame2</h1>
+          <p>Description</p>
+          <button className="start-button">Start Game</button>
+        </div>
+
+        <div>
+          <h1 className="game-logo">Minigame3</h1>
+          <p>Description</p>
+          <button className="start-button">Start Game</button>
         </div>
       </div>
+    </div>
     );
 }
   
