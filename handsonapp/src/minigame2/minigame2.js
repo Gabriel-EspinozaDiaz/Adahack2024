0f7115d7b6375b4d17c5f4e85ee78ed6103685eb
Mostafa Ebid <mebid99@gmail.com>
2024-10-19 13:19:57 +0100

    edits to comment

--- handsonapp/src/minigame2/minigame2.js
+++ handsonapp/src/minigame2/minigame2.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import './minigame2.css';
 
-// This is a javascript file, so you can write any javascript you want for your minigame here.
-
 // You will also find a css file in your minigame folder to create your own styles.
 
 // The function below is what's called a React component, essentially it just represents some html code. 
 function Minigame2() {
+
+    // This is a javascript file, so you can write any javascript you want for your minigame here.
     return (
         // Put the html for your minigame in the div bellow
       <div>
